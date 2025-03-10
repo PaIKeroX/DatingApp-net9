609485617e6b94fbf76b63580b50ed78944a2f35
PaIKeroX <traiporn.phak@gmail.com>
2025-03-11 00:24:14 +0700

    End of sections 12

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,52 +1,103 @@
-import { Component, inject, output } from '@angular/core';
+import { Component, inject, OnInit, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '@app/_services/account.service';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { NotificationService } from '@app/_services/notification.service';
+import { TextInputComponent } from "../_forms/text-input/text-input.component";
+import { DatePickerComponent } from '@app/_forms/date-picker/date-picker.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
   standalone: true,
-  imports: [CommonModule, FormsModule, MatSnackBarModule],
+  imports: [CommonModule, ReactiveFormsModule, MatSnackBarModule, TextInputComponent, DatePickerComponent],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   private accountService = inject(AccountService);
+  private fb = inject(FormBuilder);
   private notificationService = inject(NotificationService);
+  private router = inject(Router);
   cancelRegister = output<boolean>();
+  maxDate = new Date();
+  validationErrors: string[] | undefined;
 
-  model: any = {
-    username: '',
-    password: '',
-  };
+  // model: any = {
+  //   username: '',
+  //   password: '',
+  // };
+
+  registerForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18)
+  }
+
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['',Validators.required],
+      dateOfBirth: ['',Validators.required],
+      city: ['',Validators.required],
+      country: ['',Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+    });
+  
+    this.registerForm.get('password')?.valueChanges.subscribe(() => {
+      this.registerForm.get('confirmPassword')?.updateValueAndValidity();
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {isMatching: true}
+    }
+  }
 
   register() {
-    if (!this.model.username || !this.model.password) {
-      if (!this.model.username && !this.model.password) {
+    const dob = this.getDateOnly(this.registerForm.get('dateOfBirth')?.value)
+    this.registerForm.patchValue({dateOfBirth: dob});
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: _ => this.router.navigateByUrl('/members') ,
+      error: (error) => {
+        console.log(error);
+        this.validationErrors = error
+      }
+    });
+
+    const username = this.registerForm.get('username')?.value;
+    const password = this.registerForm.get('password')?.value;
+    const confirmPassword = this.registerForm.get('confirmPassword')?.value;
+
+    if (!username || !password) {
+      if (!username && !password) {
         this.notificationService.error('Username and password are required');
-      } else if (!this.model.username) {
+      } else if (!username) {
         this.notificationService.error('Username is required');
       } else {
         this.notificationService.error('Password is required');
       }
-      return;
-    }
+    return;
+  }
 
-    this.accountService.register(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.cancel();
-      },
-      error: (error) => {
-        console.log(error);
-        this.notificationService.error(error.error || 'Registration failed');
-      },
-    });
+  if (password !== confirmPassword) {
+    this.notificationService.error('Passwords do not match');
+    return;
+  }
+
   }
 
   cancel() {
     this.cancelRegister.emit(false);
+  }
+
+  private getDateOnly(dob: string | undefined) {
+    if (!dob) return;
+    return new Date(dob).toISOString().slice(0, 10);
   }
 }
